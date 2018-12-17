9e754e13fd0c6f14ea4cc69db832ce291465ae69
Pascale Anderegg <pascaleanderegg@icloud.com>
2018-12-17 19:02:06 +0100

    update hour, second, minute

--- sketch.js
+++ sketch.js
@@ -2,16 +2,13 @@ var strokeR = 204;
 var strokeG = 230;
 var strokeB = 255;
 
-angleMode(DEGREES);
-
-
 var sec = second();
 var min = minute();
-var hr = hour();
+var h = hour();
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	frameRate(1);
+	angleMode(DEGREES);
 }
 
 function draw() {
@@ -19,48 +16,34 @@ function draw() {
 	background(0);
 	noFill();
 
-	 push();
-	 //ellipse decrease to center = seconds
-	 	 for (var j = 0; j < second(); j ++){
-	 			 stroke(strokeR, strokeG, strokeB);
-	 			 ellipse(width/2, height/2, 2*j+430, 2*j+430);
+
+	translate(width/2, height/2);
+	push();
+	//ellipses rotate = seconds
+		for (var a = 0; a < second(); a ++) {
+				strokeR = map(a, 0, 59, 255, 50);
+				strokeG = map(a, 0, 59, 0, 204);
+				strokeB = map(a, 0, 59, 50, 240);
+				stroke(strokeR, strokeG, strokeB);
+				rotate(3);
+				ellipse(0, 0, 40, 500);
+		}
+		pop();
+
+
+	 //big ellipses increase = minutes
+	 	 for (var j = 0; j < minute(); j ++){
+	 			stroke(strokeR, strokeG, strokeB,60);
+	 			ellipse(0, 0, 515+j*14, 515+j*14);
 	 	 }
-		 pop();
 
 
-		 //ellipse rotate = minutes
-			 translate(width/2, height/2);
-			 for (var a = 0; a < minute(); a ++) {
-					 strokeR = map(a, 0, 59, 255, 50);
-					 strokeG = map(a, 0, 59, 0, 204);
-					 strokeB = map(a, 0, 59, 50, 240);
-					 stroke(strokeR, strokeG, strokeB);
-					 rotate(radians(3));
-					 ellipse(0, 0, 50, 600);
-			 }
+			 //filled ellipses are hours
+			 for(var r = 0; r < hour(); r ++) {
+				noStroke();
+				fill(strokeR, strokeG, strokeB);
+				rotate(15);
+				ellipse(0, 280, 24, 24);
+			}
 
-
-			 //Die Kreise, welche die Stunden darstellen sollen, sollen sich um den Kreis (innen ausgerichtet) der Sekunden anordnen
-					noStroke();
-					fill(255,255,255);
-					ellipse(0, 0, 50, 50);
-
-
-
-			/*push();
-     //squares increased = hours
-		 rectMode(CENTER);
-		 translate(3, 3);
-     for (var h = 0; h < hour(); h ++){
-         stroke(255,255,255);
-				 rect(-2.5, -2.5, 20*h, 20*h);
-     }
-		 pop();*/
 	 }
-
-
-
-/*
-	Kommentar
-
- */
